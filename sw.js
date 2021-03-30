c8e436e9625538e36524780aa2998ca53e4fa4af
steinelu <lukas.steinegger@gmail.com>
2021-03-30 12:58:11 +0200

    minor changes

--- sw.js
+++ sw.js
@@ -13,7 +13,8 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
 	event.respondWith(
-		caches.open(cacheName)
+		caches
+		.open(cacheName)
 		.then(cache => cache.match(event.request, {ignoreSearch: true}))
 		.then(response => {
 			return response || fetch(event.request)
